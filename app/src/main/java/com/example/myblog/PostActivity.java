1b5e81e482d429a6c0a09ffd613d13db18c72964
aadishjain <190050001@iitb.ac.in>
2020-06-02 11:15:42 +0530

    myblog

--- app/src/main/java/com/example/myblog/PostActivity.java
+++ app/src/main/java/com/example/myblog/PostActivity.java
@@ -3,12 +3,25 @@ package com.example.myblog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    Button post ;
+    ImageButton image ;
+    EditText name,description ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        post = (Button) findViewById(R.id.postButton) ;
+        image = (ImageButton) findViewById(R.id.imageButton) ;
+        name = (EditText) findViewById(R.id.name) ;
+        description = (EditText) findViewById(R.id.description) ;
+
     }
 }
